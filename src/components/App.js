ba347fa7c95273db494c7c4c435a53ee83c26ffc
tucker reisig <tuckerreisig@Tuckers-MacBook-Pro.local>
2020-10-17 18:42:04 -0400

    idk

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Staff from "./staff/home.js";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./customer/CustomerHome.js";
+import CustomerHome from "./customer/CustomerHome.js";
 import Menu from "./customer/Menu.js";
 import Header from "./customer/Header.js";
 import MenuItem from "./customer/MenuItem.js";
@@ -26,40 +26,41 @@ function App() {
   // and the restaurant name
 
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/"> HOME</Link>
-            </li>
-            <li>
-              <Link to="/customer">Customer</Link>
-            </li>
-            <li>
-              <Link to="/staff">Staff</Link>
-            </li>
-            <li>
-              <Link to="/customer/menu">Menu</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route exact path="/">
-            <></>
-          </Route>
-          <Route exact path="/customer">
-            <Home name="Test" />
-          </Route>
-          <Route path="/staff">
-            <Staff />
-          </Route>
-          <Route path="/customer/menu">
-            <Menu menuItems={menu} />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    // <Router>
+    //   {/* <div> */}
+    //   {/* <nav>
+    //       <ul>
+    //         <li>
+    //           <Link to="/"> HOME</Link>
+    //         </li>
+    //         <li>
+    //           <Link to="/customer">Customer</Link>
+    //         </li>
+    //         <li>
+    //           <Link to="/staff">Staff</Link>
+    //         </li>
+    //         <li>
+    //           <Link to="/customer/menu">Menu</Link>
+    //         </li>
+    //       </ul>
+    //     </nav> */}
+    //   <Switch>
+    //     <Route exact path="/">
+    //       <></>
+    //     </Route>
+    //     <Route exact path="/customer">
+    //       <CustomerHome name="Test" />
+    //     </Route>
+    //     <Route path="/staff">
+    //       <Staff />
+    //     </Route>
+    //     <Route path="/customer/menu">
+    //       <Menu name={restaurantName} menuItems={menu} />
+    //     </Route>
+    //   </Switch>
+    //   {/* </div> */}
+    // </Router>
+    <Menu name={restaurantName} menuItems={menu} />
   );
 }
 
